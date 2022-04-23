d0948550cc70b86e373a2def4137152d00784800
Axel Ezequiel Kampmann <100470703+E-K-dev@users.noreply.github.com>
2022-04-23 19:05:09 +0000

    Add files via upload

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import (
+	Flask,
+	render_template,
+	request
+);
+
+import pyshorteners
+from pyshorteners import *
+
+import os 
+from os import *
+
+from clean_function import clean # Importamos la funcion de clean.
+
+app = Flask(__name__);
+
+port = int('5000');
+
+@app.route('/' , methods=['get' , 'post'])
+def main():
+	return render_template(
+		'homepage.html',
+		Document_Title = 'Online Shortcuter'
+	);
+
+@app.route('/information' , methods=['post'])
+def info():
+	url = request.form.get('url'); # Obtenemos la data.
+	Document_Title = 'Online Shortcuter'
+	short = pyshorteners.Shortener(); # Funcion para obtener los enlaces
+	
+	tinyurl = short.tinyurl.short(f'{url}');
+	isgd = short.isgd.short(f'{url}');
+	osdb = short.osdb.short(f'{url}');
+
+	return render_template(
+		'acortado.html',
+		tinyurl=tinyurl,
+		isgd=isgd,
+		osdb=osdb,
+		Document_Title = Document_Title
+	);
+
+def run():
+	clean();
+	app.run(
+		port=port,
+		debug=True
+	);
+	pass
+
+run(); # Corremos la app.
